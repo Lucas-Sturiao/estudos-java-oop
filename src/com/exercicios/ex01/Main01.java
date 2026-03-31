68b2c91d3460531431eb578c228d7d5d5176f655
Lucas-Sturiao <dev.lucas.sturiao@gmail.com>
2026-03-31 19:46:50 -0300

    Alteração dos atributos

--- src/com/exercicios/ex01/Main01.java
+++ src/com/exercicios/ex01/Main01.java
@@ -5,8 +5,8 @@ public class Main01 {
     public static void main(String[] args) {
 
         Carro carro1 = new Carro();
-        carro1.marca = "Renaut";
-        carro1.modelo = "sandero";
+        carro1.marca = "Renault";
+        carro1.modelo = "Sandero";
         carro1.velocidade = 0;
 
         carro1.acelerar();
