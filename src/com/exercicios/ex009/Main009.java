2848bde0703590dc81166bdb8403adfe0179265a
Lucas-Sturiao <dev.lucas.sturiao@gmail.com>
2026-04-01 22:13:23 -0300

    Adição do ex009

---
+++ src/com/exercicios/ex009/Main009.java
@@ -0,0 +1,10 @@
+package com.exercicios.ex009;
+
+public class Main009 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
